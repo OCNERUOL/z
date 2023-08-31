bd33008548e03e1c1bfa5c855600fce5e5e86e4e
OCNERUOL <110502114+OCNERUOL@users.noreply.github.com>
2023-08-31 16:05:01 -0300

    Add files via upload

---
+++ p114/test1.py
@@ -0,0 +1,13 @@
+a = 33
+b = "33"
+if b > a:
+  print("b e maior que a")
+elif b == a:
+  print("a e b sao iguais")
+
+  def my_function(fname):
+    print(fname + "refsnes")
+
+  my_function("emilly")
+  my_function("adolf")
+  my_function("stalin")      
